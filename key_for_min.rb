0ae1ff0406ada42711d825f57ed9b5e4630cdc66
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-03 17:09:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,4 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-      min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
-      name_hash.find do |key, value|
-        if value < min_value
-          min_value = value
-          min_key = key
-        end
-      end
-      min_key
-  end 
-end
+      return key 
